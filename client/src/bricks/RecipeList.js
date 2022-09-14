f660bc0347344b37f5f04552bb1e54de84849bea
Pavel Nový <pavel.novy@unicorn.com>
2022-09-14 12:47:52 +0200

    "3.blok - zobrazení receptu"

--- client/src/bricks/RecipeList.js
+++ client/src/bricks/RecipeList.js
@@ -5,42 +5,83 @@ import RecipeTableList from "./RecipeTableList";
 
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
 
 import Icon from "@mdi/react";
-import { mdiTable, mdiViewGridOutline } from "@mdi/js";
-
+import { mdiCropSquare, mdiSquareSmall, mdiMagnify, mdiViewGridOutline} from "@mdi/js";
 
 function RecipeList(props){
     const [viewType, setViewType] = useState("smallDetail");
     const isSmallDetail = viewType === "smallDetail";
     const isBigDetail = viewType === "bigDetail";
+    const [searchBy, setSearchBy] = useState("");
+
+    const filteredRecipesList = useMemo(() => {
+        return props.recipeList.filter((item) => {
+            return (
+                item.name
+                    .toLocaleLowerCase()
+                    .includes(searchBy.toLocaleLowerCase())
+            );
+        });
+    }, [searchBy]);
+
+    function handleSearch(event) {
+        event.preventDefault();
+        setSearchBy(event.target["searchInput"].value);
+    }
+
+    function handleSearchDelete(event) {
+        if (!event.target.value) setSearchBy("");
+    }
+
     return(
         <div>
         <Navbar bg="light">
             <div className="container-fluid">
                 <Navbar.Brand>Zobrazení</Navbar.Brand>
-                <Button
-                    variant="outline-primary"
-                    onClick={() =>
-                        setViewType((currentState) => {
-                            if (currentState === "smallDetail") return "bigDetail";
-                            else if (currentState === "bigDetail") return "table"
-                            else return "smallDetail";
-                        })
-                    }
-                >
-                    <Icon size={1} path={isSmallDetail ? mdiTable : mdiViewGridOutline} />{" "}
-                    {isSmallDetail ? "Malí detail" : isBigDetail ? "Velký detail" : "Tabulka"}
-                </Button>
+                <div>
+                    <Form className="d-flex" onSubmit={handleSearch}>
+                        <Form.Control
+                            id={"searchInput"}
+                            style={{ maxWidth: "150px" }}
+                            type="search"
+                            placeholder="Search"
+                            aria-label="Search"
+                            onChange={handleSearchDelete}
+                        />
+                        <Button
+                            style={{ marginRight: "8px" }}
+                            variant="outline-success"
+                            type="submit"
+                        >
+                            <Icon size={1} path={mdiMagnify} />
+                        </Button>
+                        <Button
+                            variant="outline-primary"
+                            onClick={() =>
+                                setViewType((currentState) => {
+                                    if (currentState === "smallDetail") return "bigDetail";
+                                    else if (currentState === "bigDetail") return "table"
+                                    else return "smallDetail";
+                                })
+                            }
+                        >
+                            <Icon size={1} path={isSmallDetail ? mdiSquareSmall : isBigDetail ? mdiCropSquare : mdiViewGridOutline} />{" "}
+                            {isSmallDetail ? "Malí detail" : isBigDetail ? "Velký detail" : "Tabulka"}
+                        </Button>
+                    </Form>
+                </div>
             </div>
         </Navbar>
         {isSmallDetail ? (
-            <RecipeSmallDetailList recipeList={props.recipeList} />
+            <RecipeSmallDetailList recipeList={filteredRecipesList} />
         ) : isBigDetail ?
             (
-            <RecipeBigDetailList recipeList={props.recipeList} />
+            <RecipeBigDetailList recipeList={filteredRecipesList} />
         ) :
-            <RecipeTableList recipeList={props.recipeList} />
+            <RecipeTableList recipeList={filteredRecipesList} />
         }
     </div>
 );
