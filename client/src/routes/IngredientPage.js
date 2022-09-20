4b26acdf9e1732260619cc40f653ca1e0a352803
Pavel Nový <pavel.novy@unicorn.com>
2022-09-20 11:45:50 +0200

    8.block - fetch ingredientPage

--- client/src/routes/IngredientPage.js
+++ client/src/routes/IngredientPage.js
@@ -27,6 +27,20 @@ function IngredientPage(){
         });
     }, []);
 
+    useEffect(() => {
+        fetch(`http://localhost:3000/recipe/list`, {
+            method: "GET",
+        }).then(async (response) => {
+            const responseJson = await response.json();
+            if (response.status >= 400) {
+                setRecipeLoadCall({ state: "error", error: responseJson });
+            } else {
+                setRecipeLoadCall({ state: "success", data: responseJson });
+            }
+        });
+    }, []);
+
+
     function getChild() {
 
         if(ingredientsLoadCall.state === "pending"){
